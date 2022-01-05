d1669fef12723947bc79168f8915f39f6751ff04
dsh410 <dsh410@gmail.com>
2022-01-05 14:17:12 -0600

    added bootstrap and navbar.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json')
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, '/views'))
 
@@ -9,19 +11,26 @@ app.set('views',path.join(__dirname, '/views'))
 app.get("/",(req,res) => {
     res.render('home')
 })
+
 app.get('/rand',(req,res) => {
     let num = Math.floor(Math.random()*10)+1
     res.render('random',{rand:num})
 })
+
 app.get('/cats',(req,res) => {
     const cats = ['monty','ricky','bobby','mike']
     res.render('cats',{cats})
 })
-app.get('/:subreddit',(req,res) => {
-const { subreddit } = req.params;
-res.render('subreddit',{subreddit})
-})
 
+app.get('/r/:subreddit',(req,res) => {
+const { subreddit } = req.params;
+const data = redditData[subreddit]
+if(data){
+res.render('subreddit',{...data})
+}else{
+    res.render("notfound",{subreddit})
+}
+})
 
 app.listen(3000,() =>{
 console.log('listing on port 3000')
